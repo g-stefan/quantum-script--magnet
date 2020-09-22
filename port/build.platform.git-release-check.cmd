935bf88f5042e905e81f48a7148816eee1269b6e
Grigore Stefan <g_stefan@yahoo.com>
2020-09-22 15:03:23 +0300

    update

--- port/build.platform.git-release-check.cmd
+++ port/build.platform.git-release-check.cmd
@@ -7,5 +7,11 @@ SETLOCAL ENABLEDELAYEDEXPANSION
 
 echo -^> git-release-check quantum-script-extension-magnet
 
-github-release info -r quantum-script-extension-magnet
+set PROJECT=quantum-script-extension-magnet
+if not exist source\%PROJECT%.version.ini echo Error - no version & exit 1
+FOR /F "tokens=* USEBACKQ" %%F IN (`xyo-version --no-bump --get "--version-file=source\%PROJECT%.version.ini" %PROJECT%`) DO (
+	SET VERSION=%%F
+)
+
+github-release info --repo %PROJECT% --tag v%VERSION%
 
