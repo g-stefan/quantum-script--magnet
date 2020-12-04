8f79209e5545542f50aca68cb9bf1b8512930a18
Grigore Stefan <g_stefan@yahoo.com>
2020-12-04 13:56:30 +0200

    update

--- port/build.platform.github-release-check.cmd
+++ port/build.platform.github-release-check.cmd
@@ -5,7 +5,7 @@ rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-echo -^> git-release-check quantum-script-extension-magnet
+echo -^> github-release-check quantum-script-extension-magnet
 
 set PROJECT=quantum-script-extension-magnet
 if not exist source\%PROJECT%.version.ini echo Error - no version & exit 1
