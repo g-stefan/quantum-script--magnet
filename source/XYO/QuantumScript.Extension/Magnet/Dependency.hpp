2d730ba981a382b343a5a014c759f290a41e2cdb
Grigore Stefan <g_stefan@yahoo.com>
2024-01-02 22:51:53 +0200

    Update

--- source/XYO/QuantumScript.Extension/Magnet/Dependency.hpp
+++ source/XYO/QuantumScript.Extension/Magnet/Dependency.hpp
@@ -1,7 +1,7 @@
 // Quantum Script
-// Copyright (c) 2020-2023 Grigore Stefan <g_stefan@yahoo.com>
+// Copyright (c) 2020-2024 Grigore Stefan <g_stefan@yahoo.com>
 // MIT License (MIT) <http://opensource.org/licenses/MIT>
-// SPDX-FileCopyrightText: 2020-2023 Grigore Stefan <g_stefan@yahoo.com>
+// SPDX-FileCopyrightText: 2020-2024 Grigore Stefan <g_stefan@yahoo.com>
 // SPDX-License-Identifier: MIT
 
 #ifndef XYO_QUANTUMSCRIPT_EXTENSION_MAGNET_DEPENDENCY_HPP
