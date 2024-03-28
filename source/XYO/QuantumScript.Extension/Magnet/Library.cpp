f98c083249d4829c543af4c512878744ec0871cf
Grigore Stefan <g_stefan@yahoo.com>
2024-03-28 12:11:02 +0200

    Update

--- source/XYO/QuantumScript.Extension/Magnet/Library.cpp
+++ source/XYO/QuantumScript.Extension/Magnet/Library.cpp
@@ -91,7 +91,7 @@ namespace XYO::QuantumScript::Extension::Magnet {
 
 };
 
-#ifdef XYO_COMPILE_DYNAMIC_LIBRARY
+#ifdef XYO_PLATFORM_COMPILE_DYNAMIC_LIBRARY
 extern "C" XYO_QUANTUMSCRIPT_EXTENSION_MAGNET_EXPORT void quantumScriptExtension(XYO::QuantumScript::Executive *executive, void *extensionId) {
 	XYO::QuantumScript::Extension::Magnet::initExecutive(executive, extensionId);
 };
